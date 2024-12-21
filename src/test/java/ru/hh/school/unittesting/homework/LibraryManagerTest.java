c73be5de6928e60168700a793dc35ab3845f4fb6
kochachii <nbarinov956@gmail.com>
2024-12-21 21:43:02 +0300

    review fixes

--- src/test/java/ru/hh/school/unittesting/homework/LibraryManagerTest.java
+++ src/test/java/ru/hh/school/unittesting/homework/LibraryManagerTest.java
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class LibraryManagerTest {
+class LibraryManagerTest {
 
   @Mock
   protected NotificationService notificationService;
@@ -40,102 +40,65 @@ public class LibraryManagerTest {
       "bookId3, 0, 0"
   })
   @DisplayName("Получение доступных копий книг для книги с нормальным bookId")
-  void getAvailableCopies_shouldReturnExpectedCount_whenBookIdIsNormal() {
-    String bookId = "bookId";
-    int quantity = 5;
+  void getAvailableCopies_shouldReturnExpectedCount_whenBookIdIsNormal(
+      String bookId, int quantity, int expectedQuantity
+  ) {
     libraryManager.addBook(bookId, quantity);
+
     int actualQuantity = libraryManager.getAvailableCopies(bookId);
-    assertEquals(quantity, actualQuantity,
-        String.format(
-            "Для bookId \"%s\" ожидается %d копий, но получено %d",
-            bookId, quantity, actualQuantity)
-    );
+    assertEquals(expectedQuantity, actualQuantity, "Количество доступных копий не совпадает с полученным");
   }
 
   @Test
   @DisplayName("Получение доступных копий книг для книги с null bookId")
   void getAvailableCopies_shouldReturnExpectedCount_whenBookIdIsNull() {
-    String nullBookId = null;
-    int quantity = -5;
-    libraryManager.addBook(nullBookId, quantity);
-    int actualQuantity = libraryManager.getAvailableCopies(nullBookId);
-    assertEquals(quantity, actualQuantity,
-        String.format(
-            "Для bookId \"%s\" ожидается %d копий, но получено %d",
-            nullBookId, quantity, actualQuantity)
-    );
+    libraryManager.addBook(null, -5);
+
+    int actualQuantity = libraryManager.getAvailableCopies(null);
+    assertEquals(-5, actualQuantity, "Количество доступных копий не совпадает с полученным");
   }
 
   @Test
   @DisplayName("Получение доступных копий книг для книги с пустым bookId")
   void getAvailableCopies_shouldReturnExpectedCount_whenBookIdIsEmpty() {
-    String emptyBookId = "";
-    int quantity = 5;
-    libraryManager.addBook(emptyBookId, quantity);
-    int actualQuantity = libraryManager.getAvailableCopies(emptyBookId);
-    assertEquals(quantity, actualQuantity,
-        String.format(
-            "Для bookId \"%s\" ожидается %d копий, но получено %d",
-            emptyBookId, quantity, actualQuantity)
-    );
+    libraryManager.addBook("", 5);
+
+    int actualQuantity = libraryManager.getAvailableCopies("");
+    assertEquals(5, actualQuantity, "Количество доступных копий не совпадает с полученным");
   }
 
   @Test
   @DisplayName("Добавление новых книг с различными количествами")
   void addBook_shouldAddNewBooksWithVariousQuantities_whenBookIdIsNormal() {
-    String bookId = "bookId";
-    int quantity = 5;
-    int expectedQuantity = quantity;
-    libraryManager.addBook(bookId, quantity);
-    int actualQuantity = libraryManager.getAvailableCopies(bookId);
-    assertEquals(expectedQuantity, actualQuantity,
-        String.format(
-            "Для bookId \"%s\" ожидается %d копий, но получено %d",
-            bookId, expectedQuantity, actualQuantity)
-    );
-    quantity *= -2;
-    expectedQuantity += quantity;
-    libraryManager.addBook(bookId, quantity);
-    actualQuantity = libraryManager.getAvailableCopies(bookId);
-    assertEquals(expectedQuantity, actualQuantity,
-        String.format(
-            "Для bookId \"%s\" ожидается %d копий, но получено %d",
-            bookId, expectedQuantity, actualQuantity)
-    );
-    quantity = 0;
-    libraryManager.addBook(bookId, quantity);
-    actualQuantity = libraryManager.getAvailableCopies(bookId);
-    assertEquals(expectedQuantity, actualQuantity,
-        String.format(
-            "Для bookId \"%s\" ожидается %d копий, но получено %d",
-            bookId, expectedQuantity, actualQuantity)
-    );
+    libraryManager.addBook("bookId", 5);
+    libraryManager.addBook("bookId", -10);
+    libraryManager.addBook("bookId", 0);
+
+    int actualQuantity = libraryManager.getAvailableCopies("bookId");
+    assertEquals(-5, actualQuantity, "Количество доступных копий не совпадает с добавленным");
   }
 
   @Test
   @DisplayName("Попытка взять книгу неактивным пользователем")
   void borrowBook_shouldReturnFalse_whenUserIsInactive() {
-    String bookId = "bookId";
-    String userId = "userId";
-    int quantity = 5;
-    libraryManager.addBook(bookId, quantity);
-    when(userService.isUserActive(userId))
+    libraryManager.addBook("bookId", 5);
+    when(userService.isUserActive("userId"))
         .thenReturn(false);
-    boolean wasBorrowed = libraryManager.borrowBook(bookId, userId);
+
+    boolean wasBorrowed = libraryManager.borrowBook("bookId", "userId");
     assertFalse(wasBorrowed, "Метод должен вернуть false, т.к. пользователь не активен");
     verify(notificationService, times(1))
-        .notifyUser(userId, "Your account is not active.");
+        .notifyUser("userId", "Your account is not active.");
     verifyNoMoreInteractions(notificationService);
   }
 
   @Test
   @DisplayName("Попытка взять книгу, которой нет в наличии")
   void borrowBook_shouldReturnFalse_whenBookDoesNotExist() {
-    String bookId = "nonExistentBookId";
-    String userId = "userId";
-    when(userService.isUserActive(userId))
+    when(userService.isUserActive("userId"))
         .thenReturn(true);
-    boolean wasBorrowed = libraryManager.borrowBook(bookId, userId);
+
+    boolean wasBorrowed = libraryManager.borrowBook("nonExistentBookId", "userId");
     assertFalse(wasBorrowed, "Метод должен вернуть false, т.к. книги нет в наличии");
     verify(notificationService, never())
         .notifyUser(anyString(), anyString());
@@ -144,13 +107,11 @@ public class LibraryManagerTest {
   @Test
   @DisplayName("Попытка взять книгу без доступных копий")
   void borrowBook_shouldReturnFalse_whenNoAvailableCopies() {
-    String bookId = "bookId";
-    String userId = "userId";
-    int zeroQuantity = 0;
-    libraryManager.addBook(bookId, zeroQuantity);
-    when(userService.isUserActive(userId))
+    libraryManager.addBook("bookId", 0);
+    when(userService.isUserActive("userId"))
         .thenReturn(true);
-    boolean wasBorrowed = libraryManager.borrowBook(bookId, userId);
+
+    boolean wasBorrowed = libraryManager.borrowBook("bookId", "userId");
     assertFalse(wasBorrowed, "Метод должен вернуть false, т.к. нет доступных копий");
     verify(notificationService, never())
         .notifyUser(anyString(), anyString());
@@ -159,29 +120,25 @@ public class LibraryManagerTest {
   @Test
   @DisplayName("Успешное получение книги активным пользователем")
   void borrowBook_shouldReturnTrue_whenUserIsActiveAndBookIsAvailable() {
-    String bookId = "bookId";
-    String userId = "userId";
-    int quantity = 5;
-    libraryManager.addBook(bookId, quantity);
-    when(userService.isUserActive(userId))
+    libraryManager.addBook("bookId", 5);
+    when(userService.isUserActive("userId"))
         .thenReturn(true);
-    boolean wasBorrowed = libraryManager.borrowBook(bookId, userId);
+
+    boolean wasBorrowed = libraryManager.borrowBook("bookId", "userId");
     assertTrue(wasBorrowed, "Метод должен вернуть true, т.к. книга доступна и пользователь активен");
-    assertEquals(quantity - 1, libraryManager.getAvailableCopies(bookId),
+    assertEquals(4, libraryManager.getAvailableCopies("bookId"),
         "Количество доступных копий должно уменьшиться на 1");
     verify(notificationService, times(1))
-        .notifyUser(userId, "You have borrowed the book: " + bookId);
+        .notifyUser("userId", "You have borrowed the book: " + "bookId");
   }
 
   @Test
   @DisplayName("Попытка вернуть книгу, которой пользователь не брал")
   void returnBook_shouldReturnFalse_whenBookWasNotBorrowedByUser() {
-    String bookId = "bookId";
-    String userId = "userId";
-    int quantity = 5;
-    libraryManager.addBook(bookId, quantity);
+    libraryManager.addBook("bookId", 5);
     reset(notificationService);
-    boolean wasReturned = libraryManager.returnBook(bookId, userId);
+
+    boolean wasReturned = libraryManager.returnBook("bookId", "userId");
     assertFalse(wasReturned, "Метод должен вернуть false, т.к. книга не была взята пользователем");
     verify(notificationService, never())
         .notifyUser(anyString(), anyString());
@@ -190,16 +147,13 @@ public class LibraryManagerTest {
   @Test
   @DisplayName("Попытка вернуть книгу, взятую другим пользователем")
   void returnBook_shouldReturnFalse_whenBookWasBorrowedByAnotherUser() {
-    String bookId = "bookId";
-    String userId = "userId";
-    String otherUserId = "otherUserId";
-    int quantity = 5;
-    libraryManager.addBook(bookId, quantity);
-    when(userService.isUserActive(otherUserId))
+    libraryManager.addBook("bookId", 5);
+    when(userService.isUserActive("otherUserId"))
         .thenReturn(true);
-    libraryManager.borrowBook(bookId, otherUserId);
+    libraryManager.borrowBook("bookId", "otherUserId");
     reset(notificationService);
-    boolean wasReturned = libraryManager.returnBook(bookId, userId);
+
+    boolean wasReturned = libraryManager.returnBook("bookId", "userId");
     assertFalse(wasReturned, "Метод должен вернуть false, т.к. книга была взята другим пользователем");
     verify(notificationService, never())
         .notifyUser(anyString(), anyString());
@@ -208,28 +162,26 @@ public class LibraryManagerTest {
   @Test
   @DisplayName("Успешное возвращение книги пользователем")
   void returnBook_shouldReturnTrue_whenBookIsBorrowedByUser() {
-    String bookId = "bookId";
-    String userId = "userId";
-    int quantity = 5;
-    libraryManager.addBook(bookId, quantity);
-    when(userService.isUserActive(userId))
+    libraryManager.addBook("bookId", 5);
+    when(userService.isUserActive("userId"))
         .thenReturn(true);
-    libraryManager.borrowBook(bookId, userId);
+    libraryManager.borrowBook("bookId","userId");
     reset(notificationService);
-    boolean wasReturned = libraryManager.returnBook(bookId, userId);
+
+    boolean wasReturned = libraryManager.returnBook("bookId", "userId");
     assertTrue(wasReturned, "Метод должен вернуть true, т.к. книга успешно возвращена пользователем");
-    assertEquals(quantity, libraryManager.getAvailableCopies(bookId),
+    assertEquals(5, libraryManager.getAvailableCopies("bookId"),
         "Количество доступных копий не должно отличаться от исходного");
     verify(notificationService, times(1))
-        .notifyUser(userId, "You have returned the book: " + bookId);
+        .notifyUser("userId", "You have returned the book: " + "bookId");
   }
 
   @ParameterizedTest(name = "calculateDynamicLateFee({0}, {1}, {2}) should throw IllegalArgumentException")
   @CsvSource({
-      "-1, false, false, 0.50",
-      "-2, false, true, 1.00",
-      "-3, true, false, 1.50",
-      "-4, true, true, 2.00",
+      "-1, false, false",
+      "-2, false, true",
+      "-3, true, false",
+      "-4, true, true",
   })
   @DisplayName("Попытка подсчета штрафа, но дни просрочки отрицательны")
   void calculateDynamicLateFee_shouldThrowException_whenOverdueDaysIsNegative(
